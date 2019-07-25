d20e8d86d7f9dee6384353af18f2ed687086b9b7
Agustin C. Caminero <accaminero@scc.uned.es>
2019-07-25 15:49:23 +0200

    jupyter

--- startup.sh
+++ startup.sh
@@ -2,4 +2,5 @@
 
 echo "###################### Starting Jupyter Notebook ######################"
 jupyter notebook --port 8889 --notebook-dir='/media/notebooks' --no-browser  --allow-root  &
+echo "###################### Started Jupyter Notebook ######################"
 
